49561c0b27350c03f22c46964a1a116e55a1dd4f
TripleC3 <70118538+TripleC3@users.noreply.github.com>
2021-04-10 03:38:20 +0200

    Update wifi_pass_extractor.py

--- wifi_pass_extractor.py
+++ wifi_pass_extractor.py
@@ -5,9 +5,8 @@ comando = subprocess.check_output('netsh wlan show profiles').decode('utf-8').sp
 for line in comando:
     if 'Perfil de todos los usuarios' in line:
         wifis.append(line.split(':')[1][1:-1])
+        
 print('Claves encontradas:\n')
-# print(subprocess.check_output(f'netsh wlan show profile "{wifis[0]}" key=clear'))
-# print(subprocess.check_output(f'netsh wlan show profile "{wifis[0]}" key=clear').decode('utf-8', errors='ignore'))
 for wifi in wifis:
     comando = subprocess.check_output(f'netsh wlan show profile "{wifi}" key=clear').decode('utf-8', errors='ignore').split('\n')
     for line in comando:
